de00c3a06cd27692503ed89f61d24f7dce5be5b0
ivan oparin <ivanopa.rin@yandex.ru>
2023-05-19 16:34:45 +0300

    back add base functional

--- DailyPlannerBack/DailyPlannerBack/Controllers/TasksController.cs
+++ DailyPlannerBack/DailyPlannerBack/Controllers/TasksController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using DailyPlannerBack.Models;
+using DailyPlannerBack.ViewModels;
 
 namespace DailyPlannerBack.Controllers
 {
@@ -15,6 +16,49 @@ namespace DailyPlannerBack.Controllers
         {
             this.db = db;
             this.logger = logger;
+        }
+
+        [HttpGet("[Action]")]
+        public ActionResult<IEnumerable<Models.Task>> GetTasks(TaskFiltrationViewModel? filtration)
+        {
+            if(filtration == null)
+            {
+                return db.Tasks;
+            }
+
+            IEnumerable<Models.Task> tasks = db.Tasks.ToArray();
+            if(filtration.Title != null)
+            {
+                tasks = tasks.Where(task => task.Title.ToLower() == filtration.Title.ToLower());
+            }
+            if(filtration.Start != null)
+            {
+                tasks = tasks.Where(task => task.Start >= filtration.Start);
+            }
+            if(filtration.End != null)
+            {
+                tasks = tasks.Where(task => task.End <= filtration.End);
+            }
+
+            return Ok(tasks);
+        }
+
+        [HttpPost("[Action]")]
+        public ActionResult CreateTask(CreateTaskViewModel createVM)
+        {
+            Models.Task task = new Models.Task()
+            {
+                Title = createVM.Title,
+                Message = createVM.Message,
+                Start = createVM.Start,
+                End = createVM.End,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            db.Tasks.Add(task);
+            db.SaveChanges();
+
+            return Ok(task);
         }
     }
 }
